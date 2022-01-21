05582053b9b471702e31dbecf30fc90ea380ac5d
aardoiz <aardoiz14@gmail.com>
2022-01-21 11:45:16 +0100

    :art: black

--- preprocess.py
+++ preprocess.py
@@ -1,38 +1,32 @@
-#libraries for text preprocessing
+# libraries for text preprocessing
 import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 
-nltk.download('wordnet')
-nltk.download('stopwords')
+nltk.download("wordnet")
+nltk.download("stopwords")
+nltk.download("omw-1.4")
+
 
 def preprocess(text):
     """
-    Takes out stopwords
-    Takes out special char
-    Takes out puntuaction and digits
-    Pone to en minúsculas
+    For any text, this function does the following:
+        Replace complex expression for unicode chars < >.
+        Take out puntuaction and digits.
+        Convert all the character to lowercase.
+        Take out stopwords and get the stemma for each word.
     """
-    #define stopwords
-    stop_words = set(stopwords.words("spanish")) 
-    #Remove punctuations
-    text = re.sub('[^a-zA-ZíóúáéñÑÁÉÍÓÚü]', ' ', text)
-    #Convert to lowercase
+
+    stop_words = set(stopwords.words("spanish"))
+    text = re.sub("&lt;/?", "<", text)
+    text = re.sub("&gt;", ">", text)
+    text = re.sub("(\d|\.|,)+", " ", text)
     text = text.lower()
-    #remove tags
-    text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
-    # remove special characters and digits
-    text=re.sub("(\\d|\\W)+"," ",text) # CAMBIAR A \d \W
-    ##Convert to list from string
+
     text = text.split()
-    ##Stemming
-    snowball = SnowballStemmer('spanish')
-    text = [snowball.stem(word) for word in text if not word in stop_words]
-    #Lemmatisation
-    lem = WordNetLemmatizer()
-    text = [lem.lemmatize(word) for word in text if not word in  stop_words] 
-    text = " ".join(text) 
-    
+    snowball = SnowballStemmer("spanish")
+    text = " ".join([snowball.stem(word) for word in text if not word in stop_words])
+
     return text
