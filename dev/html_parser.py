eb565ed1976e914e84629c6988f93286da9ab455
aardoiz <aardoiz14@gmail.com>
2022-01-23 20:04:03 +0100

    :construction: in development

--- dev/html_parser.py
+++ dev/html_parser.py
@@ -1,12 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import es_core_news_sm
 
-nlp = es_core_news_sm.load()
 text = "mi perro es super de Amazon"
 
-doc = nlp(text)
-for t in doc:
-    print(t.lemma_)
+
 """r = requests.get('https://ariesco.github.io/OIM/docs/intro.html')
 soup = BeautifulSoup(r.text, 'lxml')"""
