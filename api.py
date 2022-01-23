07c3000a20ab4a38497d241988109dc645ae11cf
aardoiz <aardoiz14@gmail.com>
2022-01-23 20:16:52 +0100

    :recycle: change project structure

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ from rank_bm25 import BM25Okapi
 from sentence_transformers import CrossEncoder, SentenceTransformer, util
 from torch import cuda, device
 
-from text_process import Preprocess
+from preprocessing.models.text_process import Preprocess
 
 
 class Search(BaseModel):
