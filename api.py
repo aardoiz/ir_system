44b55826337142691535e46d94266e3e4f92c59b
aardoiz <aardoiz14@gmail.com>
2022-01-23 21:14:18 +0100

    :zap: stable  refactorized version

--- api.py
+++ api.py
@@ -1,9 +1,6 @@
-import pickle
-
 import numpy as np
 import torch
 import uvicorn
-from environs import Env
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -13,46 +10,12 @@ from sentence_transformers import CrossEncoder, SentenceTransformer, util
 from torch import cuda, device
 
 from preprocessing.models.text_process import Preprocess
+from database.access_db import Get_all_data
 
 
 class Search(BaseModel):
     query: str
 
-
-def read_pickle(pickle_file: str, object_type: str):
-    """
-    Read a .pkl file and loads into memory.
-    It's used for acessing the corpus of the IR system
-    """
-    with open(pickle_file, "rb") as fich:
-        if object_type == "tensor":
-            read_file = torch.load(fich, map_location="cpu")
-        elif object_type == "list":
-            read_file = pickle.load(fich)
-        else:
-            raise ("Error: No available object type")
-    return read_file
-
-
-# Environs
-env = Env()
-env.read_env()
-
-super_path = "data/"
-
-embeddings = env.str("EMBEDDINGS", f"{super_path}pickle/embeddings.pt")
-sentences = env.str("SENTENCES", f"{super_path}pickle/sentences.pkl")
-paragraphs = env.str("PARAGRAPHS", f"{super_path}pickle/paragraphs.pkl")
-documents = env.str("DOCUMENTS", f"{super_path}pickle/documents.pkl")
-
-sentence_transformers_model = env.str(
-    "SENTENCE_TRANSFORMERS_MODEL",
-    "eduardofv/stsb-m-mt-es-distiluse-base-multilingual-cased-v1",
-)
-
-cross_encoder_model = env.str(
-    "CROSS_ENCODER_MODEL", "cross-encoder/ms-marco-MiniLM-L-2-v2"
-)
 
 # FastAPI  Settings
 app = FastAPI()
@@ -69,20 +32,20 @@ print(f"Device selected: {device}")
 
 
 # Semantic Searcher
-all_embedding_from_source = read_pickle(embeddings, "tensor")
-all_sentences_from_source = read_pickle(sentences, "list")
-all_documents_from_source = read_pickle(documents, "list")
-all_paragraphs_from_source = read_pickle(paragraphs, "list")
+all_documents_ , all_paragraphs_, all_sentences_ ,all_embedding_  = Get_all_data()
 
+
+sentence_transformers_model = "eduardofv/stsb-m-mt-es-distiluse-base-multilingual-cased-v1"
+cross_encoder_model = "cross-encoder/ms-marco-MiniLM-L-2-v2"
 model = SentenceTransformer(sentence_transformers_model)
 cross = CrossEncoder(cross_encoder_model)
 
 # Okapi BM25
 tokenized_corpus_sentence = [
-    Preprocess(doc).split(" ") for doc in all_sentences_from_source
+    Preprocess(doc).split(" ") for doc in all_sentences_ 
 ]
 tokenized_corpus_paragraph = [
-    Preprocess(doc).split(" ") for doc in all_paragraphs_from_source
+    Preprocess(doc).split(" ") for doc in all_paragraphs_ 
 ]
 
 bm25_sentence = BM25Okapi(tokenized_corpus_sentence)
@@ -90,8 +53,6 @@ bm25_parragraph = BM25Okapi(tokenized_corpus_paragraph)
 
 
 # Main methods
-
-
 @app.get("/")
 def root():
     return {"message": "Hello"}
@@ -106,7 +67,7 @@ def compute_sbert(search: Search) -> dict:
     - Get the 7 phrases which similarity scored highest.
     """
     input_embeddings = model.encode([search.query], convert_to_tensor=True)
-    cosine_scores = util.pytorch_cos_sim(input_embeddings, all_embedding_from_source)
+    cosine_scores = util.cos_sim(input_embeddings, all_embedding_ )
     best = torch.topk(cosine_scores, 7)
 
     output = []
@@ -117,10 +78,10 @@ def compute_sbert(search: Search) -> dict:
         idx = int(idx)
         output.append(
             {
-                "Oración": all_sentences_from_source[idx],
-                "Párrafo": all_paragraphs_from_source[idx],
+                "Oración": all_sentences_ [idx],
+                "Párrafo": all_paragraphs_ [idx],
                 "Score": score,
-                "Documento": all_documents_from_source[idx],
+                "Documento": all_documents_ [idx],
             },
         )
 
@@ -161,10 +122,10 @@ def compute_bm(search: Search) -> dict:
             continue
         output.append(
             {
-                "Oración": all_sentences_from_source[idx],
-                "Párrafo": all_paragraphs_from_source[idx],
+                "Oración": all_sentences_ [idx],
+                "Párrafo": all_paragraphs_ [idx],
                 "Score": score,
-                "Documento": all_documents_from_source[idx],
+                "Documento": all_documents_ [idx],
             }
         )
 
@@ -208,10 +169,10 @@ def compute_crossencoder(search: Search) -> dict:
             continue
         output.append(
             {
-                "Oración": all_sentences_from_source[idx],
-                "Párrafo": all_paragraphs_from_source[idx],
+                "Oración": all_sentences_ [idx],
+                "Párrafo": all_paragraphs_ [idx],
                 "Score": score,
-                "Documento": all_documents_from_source[idx],
+                "Documento": all_documents_ [idx],
             },
         )
 
