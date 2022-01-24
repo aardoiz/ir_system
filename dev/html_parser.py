d1335315f8059038dda8826e2d9143c60f911194
aardoiz <aardoiz14@gmail.com>
2022-01-24 18:49:59 +0100

    :recycle: new project structure

--- dev/html_parser.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-text = "mi perro es super de Amazon"
-
-
-"""r = requests.get('https://ariesco.github.io/OIM/docs/intro.html')
-soup = BeautifulSoup(r.text, 'lxml')"""
