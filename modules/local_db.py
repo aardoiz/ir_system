48438a3eb44a3da02a9ce1edf30000c613919d4d
Alfonso Ardoiz <aardoiz14@gmail.com>
2022-04-07 23:47:40 +0200

    :sparkles: Select local data or sample data

--- modules/local_db.py
+++ modules/local_db.py
@@ -3,11 +3,17 @@ import pickle
 from modules.models.text_process import eval_preproces
 
 
-with open('eval/data/datos_sqac.pkl', 'rb') as f:
+
+with open('./eval/data/datos_sqac.pkl', 'rb') as f:
     data = pickle.load(f)
 
-"""with open('eval/data/embeddings_sqac.pkl', 'rb') as fl:
-    emb = pickle.load(fl)"""
+try:
+    with open('./data/pickle/document_list.pkl', 'rb') as f:
+        data = pickle.load(f)
+        print('USING LOCAL DATA')
+
+except Exception as err:
+    print('USING EXAMPLE DATA')
 
 
 def Get_local_data() -> Union[List[str], List[str], List[str], List[List]]:
@@ -18,18 +24,18 @@ def Get_local_data() -> Union[List[str], List[str], List[str], List[List]]:
     list_of_documents = []
     list_of_paragraphs = []
     list_of_sentences = []
-    list_of_embeddings = []
     list_of_questions = []
 
     for i,document in enumerate(data):
         list_of_documents.append(i)
         list_of_paragraphs.append(eval_preproces(document["title"]))
         list_of_sentences.append(eval_preproces(document["content"]))
-        list_of_embeddings.append([])
+        try:
+            list_of_questions.append(document["question"])
+        except:
+            list_of_questions.append([])
 
-        list_of_questions.append(document["question"])
-
-    return list_of_documents, list_of_paragraphs, list_of_sentences, list_of_embeddings, list_of_questions
+    return list_of_documents, list_of_paragraphs, list_of_sentences, list_of_questions
 
 
 
